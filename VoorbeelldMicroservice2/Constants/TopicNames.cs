4f52c415d8c902fe7c9c0c31f3dfb102c96f9a63
Maarten van der Heijden <djbatcat@gmail.com>
2019-11-21 17:07:30 +0100

    develop: Update versions

--- VoorbeelldMicroservice2/Constants/TopicNames.cs
+++
@@ -1,7 +0,0 @@
-namespace VoorbeeldMicroService.Constants
-{
-    public static class TopicNames
-    {
-        public const string MvmPolisbeheerPolisToegevoegd = "MVM.Polisbeheer.PolisToegevoegd";
-    }
-}
--- VoorbeelldMicroservice2/Models/Polis.cs
+++
@@ -1,8 +0,0 @@
-namespace VoorbeeldMicroService.Models
-{
-    public class Polis
-    {
-        public long Id { get; set; }
-        public string Klantnaam { get; set; }
-    }
-}
