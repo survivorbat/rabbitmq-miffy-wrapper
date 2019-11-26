92a772ba6ef8d103770e322c968092ddc3f28a42
Maarten van der Heijden <djbatcat@gmail.com>
2019-11-26 14:28:31 +0100

    develop: Add internals visibility

--- Minor.Miffy.RabbitMQBus/Constants/EnvVarNames.cs
+++ Minor.Miffy.RabbitMQBus/Constants/EnvVarNames.cs
@@ -3,9 +3,9 @@ namespace Minor.Miffy.RabbitMQBus.Constants
     /// <summary>
     /// Names of environment variables being used
     /// </summary>
-    public static class EnvVarNames
+    internal static class EnvVarNames
     {
-        public static readonly string BrokerConnectionString = "BROKER_CONNECTION_STRING";
-        public static readonly string BrokerExchangeName = "BROKER_EXCHANGE_NAME";
+        internal const string BrokerConnectionString = "BROKER_CONNECTION_STRING";
+        internal const string BrokerExchangeName = "BROKER_EXCHANGE_NAME";
     }
 }
---
+++ Minor.Miffy.RabbitMQBus/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Minor.Miffy.RabbitMQBus.Test")]
+[assembly:InternalsVisibleTo("Minor.Miffy.RabbitMQBus.Test.Integration")]
