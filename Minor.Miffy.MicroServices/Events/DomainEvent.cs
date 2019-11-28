d7dbf46bef4925fc88962429e55deede1be92660
Maarten van der Heijden <djbatcat@gmail.com>
2019-11-28 16:43:06 +0100

    develop: Add modifiers

--- Minor.Miffy.MicroServices/Events/DomainEvent.cs
+++ Minor.Miffy.MicroServices/Events/DomainEvent.cs
@@ -12,30 +12,29 @@ namespace Minor.Miffy.MicroServices.Events
         /// The Topic is used by the underlying protocol to route events to subscribers
         /// </summary>
         [JsonProperty]
-        public string Topic { get; internal set; }
+        protected internal string Topic { get; internal set; }
 
         /// <summary>
         /// The Timestamp is set to the creation time of the domain event.
         /// </summary>
         [JsonProperty]
-        public long Timestamp { get; internal set; }
+        protected internal long Timestamp { get; internal set; }
 
         /// <summary>
         /// The ID uniquely identifies the domain event.
         /// </summary>
         [JsonProperty]
-        public Guid Id { get; internal set; }
+        protected internal Guid Id { get; internal set; }
 
         /// <summary>
         /// Creates a domain event by setting the topic and generating a timestamp.
         /// </summary>
         /// <param name="topic">The topic should be of the format domain.eventname</param>
-        protected DomainEvent(string topic)
+        protected internal DomainEvent(string topic)
         {
             Topic = topic;
             Timestamp = DateTime.Now.Ticks;
             Id = Guid.NewGuid();
         }
     }
-
 }
