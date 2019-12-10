df337db7992a2c56bde4fb1a614d983bcfce7f1b
Maarten van der Heijden <djbatcat@gmail.com>
2019-12-10 19:48:41 +0100

    develop: Add event publisher string version

--- Minor.Miffy.MicroServices/Events/EventPublisher.cs
+++ Minor.Miffy.MicroServices/Events/EventPublisher.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json;
@@ -33,21 +34,43 @@ namespace Minor.Miffy.MicroServices.Events
         /// </summary>
         public void Publish(DomainEvent domainEvent)
         {
-            _logger.LogTrace($"Publishing domain event with type {domainEvent.GetType().Name} and ID {domainEvent.Id}");
-            
+            _logger.LogTrace($"Publishing domain event with type {domainEvent.Type} and ID {domainEvent.Id}");
+
             string json = JsonConvert.SerializeObject(domainEvent);
-            
+
             _logger.LogDebug($"Publishing domain event {domainEvent.Id} with body: {json}");
-            
+
             EventMessage message = new EventMessage
             {
                 Timestamp = domainEvent.Timestamp,
                 Topic = domainEvent.Topic,
                 CorrelationId = domainEvent.Id,
-                EventType = domainEvent.GetType().Name,
+                EventType = domainEvent.Type,
                 Body = Encoding.Unicode.GetBytes(json)
             };
-            
+
+            _sender.SendMessage(message);
+        }
+
+        /// <summary>
+        /// Publish a raw message to the bus with explicit values
+        ///
+        /// In case you want to publish a 'normal' event you're strongly encouraged to use the
+        /// DomainEvent variant.
+        /// </summary>
+        public void Publish(long timeStamp, string topic, Guid correlationId, string eventType, string body)
+        {
+            _logger.LogTrace($"Publishing domain event with type {eventType} and ID {correlationId.ToString()} and body {body}");
+
+            EventMessage message = new EventMessage
+            {
+                Timestamp = timeStamp,
+                Topic = topic,
+                CorrelationId = correlationId,
+                EventType = eventType,
+                Body = Encoding.Unicode.GetBytes(body)
+            };
+
             _sender.SendMessage(message);
         }
     }
