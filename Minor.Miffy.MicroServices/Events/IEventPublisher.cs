32fbc2c69a6d4f8a80171b111d4ea28017904009
Maarten van der Heijden <djbatcat@gmail.com>
2019-12-16 07:28:15 +0100

    master: Add overloaded method to interface

--- Minor.Miffy.MicroServices/Events/IEventPublisher.cs
+++ Minor.Miffy.MicroServices/Events/IEventPublisher.cs
@@ -1,7 +1,10 @@
-﻿namespace Minor.Miffy.MicroServices.Events
+﻿using System;
+
+namespace Minor.Miffy.MicroServices.Events
 {
     public interface IEventPublisher
-    { 
+    {
         void Publish(DomainEvent domainEvent);
+        void Publish(long timeStamp, string topic, Guid correlationId, string eventType, string body);
     }
 }
